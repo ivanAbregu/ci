812d1a5d4966128fc8a0fbf3c01bafdadcdd47d8
ivanAbregu <ivan.abregu@gmail.com>
2020-02-08 14:28:06 -0300

    added test

--- test_sum.py
+++ test_sum.py
@@ -5,7 +5,8 @@ from sum import custom_sum
 class MyTestCase(unittest.TestCase):
     def test_custom_sum(self):
         self.assertEqual(custom_sum(1, 1), 2)
-
+    def test_sum_big(self):
+        self.assertEqual(custom_sum(5000, 5000), 10000)
 
 if __name__ == '__main__':
     unittest.main()
