9191e13a5829f9db2054772bc51e6405816de8db
ivanAbregu <ivan.abregu@gmail.com>
2020-03-08 17:50:16 -0300

    mult (#5) lalala title
    * mult
    
    * rest
    
    * increase
    
    * decrease
    
    * decreaseqwqw
    
    coments

--- sum.py
+++ sum.py
@@ -1,3 +1,21 @@
-
+# a comment
 def custom_sum(x, y):
     return x + y
+
+# a comment
+def custom_resrt(x, y):
+    return x - y
+
+# a comment
+def custom_mult(x, y):
+    return x * y
+
+# a comment
+def custom_increase(x):
+    return x + 1
+
+
+# a comment
+# a comment
+def custom_decrease(x):
+    return x - 1
